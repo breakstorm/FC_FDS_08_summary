03d9030fa862b7e7117f26d7ef308e79d5ea43a0
김휘겸 <breakstorm00@gmail.com>
2018-02-21 10:16:54 +0900

    Update todoV0 script file(투두리스트 V0 스크립트 파일 수정)

--- hwigyum/todoV0/script.js
+++ hwigyum/todoV0/script.js
@@ -1,4 +1,4 @@
-const todos = [
+let todos = [
   { id: 3, content: "HTML", completed: false },
   { id: 2, content: "CSS", completed: true },
   { id: 1, content: "Javascript", completed: false }
@@ -13,22 +13,27 @@ document.addEventListener('DOMContentLoaded', (e)=>{
   const ulTarget = document.querySelector('#todo-list');
 
   if(todos.length){
-    todos.forEach((v, i, a)=>{ insertList(v.content); });
+    todos.forEach((v, i, a)=>{ viewInsertList(v.content); });
   }
 
   inputTarget.addEventListener('keyup', function(v) {
     if(v.keyCode === 13 && v.target.value ) {
-      insertList(v.target.value);
+      modelInsertList(v.target.value);
+      // viewInsertList(v.target.value);
       v.target.value = '';
     }
   });
 
   ulTarget.addEventListener("click", function(v) {
-    if(v.target.nodeName === 'LI') deleteList(v);    
+    if(v.target.nodeName === 'LI'){
+      modelDeleteList(v.target.innerText);
+      // viewDeleteList(v);
+    }     
   });
 })
 
-function insertList(item) {
+
+function viewInsertList(item) {
   // li요소 추가
   
   const ulTarget = document.querySelector('#todo-list');
@@ -36,11 +41,43 @@ function insertList(item) {
   ulTarget.insertAdjacentHTML('beforeend', template);
 }
 
-function deleteList(v) {
+function viewDeleteList(v) {
   // 선택된 요소가 무엇인지 찾아서 선택. 
   // 부모요소 선택.
   // 요소 제거.
 
   const parent = v.target.parentElement;
   parent.removeChild(v.target);
+}
+
+function viewAllDeleteList() {
+  // debugger;
+  let ulTarget = document.querySelector('#todo-list');
+  let childCount = ulTarget.childElementCount;
+  for(let i = 0; i < childCount; i++){
+    ulTarget.removeChild(ulTarget.firstElementChild);
+  }
+  // ulTarget.childNodes.forEach(v => { ulTarget.removeChild(v) });
+}
+
+function viewRerendering() {
+  viewAllDeleteList();
+  todos.forEach(v => { viewInsertList(v.content) });
+}
+
+function modelInsertList(item){
+  // template : { id: 3, content: "HTML", completed: false }
+  const template = {id: 'test', content: item, completed: false}
+  todos = todos.concat([template]);
+
+  // render 호출(전체삭제, 전체호출)
+  viewRerendering();
+}
+
+function modelDeleteList(item) {
+  //todos에서 해당 되는 아이템 이외의 것만 살리기
+  todos = todos.filter(v => { if(v.content !== item) return v})
+
+  // render 호출
+  viewRerendering();
 }
